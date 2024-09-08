3a33d46f77b59ab79fe7a92a7e76da68098987d6
Joe Telila <joetelila@Joes-Mac-mini.local>
2024-09-08 16:43:27 -0400

    rename train add result folder

--- train.py
+++ train.py
@@ -104,7 +104,7 @@ def parse_arguments():
     # Define command-line arguments
     parser.add_argument('-clients', '--clients', default=10, type=str, help='Total number of clients in FL')
     parser.add_argument('-batchsize', '--batchsize', default=32, type=str, help='Total number of clients in FL')
-    parser.add_argument('-iid', '--isiid', default=True, type=bool, help='Total number of clients in FL')
+    parser.add_argument('-iid', '--isiid', default=False, type=bool, help='Total number of clients in FL')
     parser.add_argument('-seed', '--seed', default=42, type=bool, help='Total number of clients in FL')
     parser.add_argument('-alpha', '--alpha', default=0.07, type=int, help='Dritchelet alpha value')
     parser.add_argument('-log', '--log', default=True, type=bool, help='log all outputs')
@@ -113,9 +113,9 @@ def parse_arguments():
     parser.add_argument('-epochs', '--epoch', default=10, type=int, help='total epoch per clients')
     parser.add_argument('-lr', '--lr', default=0.001, type=float, help='learning rate')
     parser.add_argument('-device', '--device', default='mps', type=str, help='device to train the model')
-    parser.add_argument('-round', '--round', default=10, type=int, help='total number of global rounds')
-    parser.add_argument('-clsplit', '--clsplit', default=0.8, type=float, help='client split for training')
-    parser.add_argument('-data', '--data', default='mnist', type=str, help='model to train')
+    parser.add_argument('-round', '--round', default=20, type=int, help='total number of global rounds')
+    parser.add_argument('-clsplit', '--clsplit', default=0.99, type=float, help='client split for training')
+    parser.add_argument('-data', '--data', default='cifar10', type=str, help='model to train')
 
     # Parse arguments
     args = parser.parse_args()
